f512bb6475b62650870c0342ecef4d62a1f38f2d
Christian Rutz <chrisrutz@gmail.com>
2024-07-23 22:58:04 +0200

    remove RVM and Ruby installation from install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,6 @@ echo "Setting up your Mac..."
 # XCode Command Line Tools
 xcode-select --install &> /dev/null
 
-# installing rvm & ruby 2.7.0
-gpg --quiet --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable --with-default-gems='bundler awesome_print'
-
-rvm install 2.7.0
-rvm --default use 2.7.0
-
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
