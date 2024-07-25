2c2f331bbf94790ba611bc28176680a9c6388cdb
Christian Rutz <hi@chrisrutz.dev>
2024-07-25 12:00:21 +0200

    update install.sh script with various improvements and updates

--- install.sh
+++ install.sh
@@ -2,26 +2,77 @@
 
 echo "Setting up your Mac..."
 
-# XCode Command Line Tools
-xcode-select --install &> /dev/null
+set -e
 
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+print_message()  {
+  echo "$1..."
+}
 
-# Update Homebrew recipes
-brew update
+prompt_override() {
+  local file=$1
+  if [ -f "$file" ]; then
+    read -p "$file already exists. Do you want to override it? (y/n): " choice
+    if [ "$choice" != "y" ]; then
+      mv "$file" "${file}.bak"
+      echo "Backup of $file created as ${file}.bak"
+    fi
+  fi
+}
 
-# Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
-
-# Make ZSH the default shell environment
+print_message "Make ZSH the default shell environment"
 chsh -s $(which zsh)
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+print_message "Install Xcode Command Line Tools"
+xcode-select --install &> /dev/null
+
+print_message "Check Homebrew installation"
+if ! command -v brew >/dev/null 2>&1; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+print_message "Update Homebrew formulae"
+brew update
+
+print_message "Install Brewfile bundle"
+brew tap homebrew/bundle
+brew bundle
+prompt_override ~/.Brewfile && cp Brewfile ~/.Brewfile
+
+print_message "Install Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+print_message "Install iTerm2 shell integration"
+sh -c "$(curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh)"
+print_message "Install NVM"
+sh -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh)"
+
+print_message "Install latest ruby version using rbenv"
+eval "$(rbenv init -)"
+latest_ruby=$(rbenv install -l | grep -v - | tail -1)
+rbenv install $latest_ruby
+rbenv global $latest_ruby
+
+print_message "Copy dotfiles"
+for file in .{gemrc,irbrc,pryrc,zshrc,gitconfig,gitignore_global}; do
+  prompt_override ~/$file && cp $file ~/$file
+done
+
+print_message "Authenticate with GitHub"
+gh auth login
+git config --global user.name "$(gh api user --jq '.name')"
+git config --global user.email "$(gh api user --jq '.email')"
+
+print_message "Source zshrc"
+source ~/.zshrc
+
+print_message "Install Ruby gems"
+gem install bundler pry
+
+print_message "Install Node.js"
+nvm install --lts
+nvm alias default lts/*
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
-source .macos
+# source .macos
+
+echo "Done."
